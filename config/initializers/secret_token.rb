bab6fa0ac7b227e15e746a35fd4c7a2a49ffd8dd
Neilda <neilda@neildapacquing.com>
2014-12-18 22:00:23 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Awesomeness::Application.config.secret_key_base = '07e2e5beb1a1bc0ccf18380a3d2821e2e8b09a0783618966695fa27ac0c04fcd86b832f126b7d89f315208e684c981de5753409fb0da5db62b3081b47351d50a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Awesomeness::Application.config.session_store :cookie_store, key: '_awesomeness_session'
